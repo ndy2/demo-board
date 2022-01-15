6f216daf2ae0e8f08d650171d0b49a825044f5cd
ndy2 <emrdbs12@gmail.com>
2022-01-15 20:09:43 +0900

    swagger 설정 추가

--- src/main/java/com/example/demoboard/DemoBoardApplication.java
+++ src/main/java/com/example/demoboard/DemoBoardApplication.java
@@ -2,12 +2,12 @@ package com.example.demoboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableSwagger2
 public class DemoBoardApplication {
 
     public static void main(String[] args) {
